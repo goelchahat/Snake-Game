e74a793847d064925e2a9d5c09a8d770cfd4d692
Chahat Goel <chahatgoel98@gmail.com>
2018-04-05 22:24:36 +0530

    learning the basics

--- Game.py
+++ Game.py
@@ -49,5 +49,34 @@ def window () :
     tscore.speed(0)
     tscore.goto(100, -250)
     tscore.write("Score:" + str(a[0]), align="center", font=(10))
-    
+
+    while x > -210 and x < 210 and y > -210 and y < 210:
+        if foodcoord[2] == 0:
+            food(tfood)
+            foodcoord[2] = 1
+        turtle.onkey(u, "Up")
+        turtle.onkey(l, "Left")
+        turtle.onkey(r, "Right")
+        turtle.onkey(d, "Down")
+        turtle.listen()
+        move()
+        x = turtle.xcor()
+        y = turtle.ycor()
+        if x > foodcoord[0] * 20 - 5 and x < foodcoord[0] * 20 + 5 and y > foodcoord[1] * 20 - 5 and y < foodcoord[
+            1] * 20 + 5:
+            foodcoord[2] = 0
+            tfood.clear()
+            a[0] += 1
+            tscore.clear()
+            tscore.write("Score:" + str(a[0]), align="center", font=(10))
+         if len(cpos) > 1:
+             for i in range(1, len(cpos)):
+                if x < cpos[i][0] + 5 and x > cpos[i][0] - 5 and y < cpos[i][1] + 5 and y > cpos[i][1] - 5:
+                        tscore.clear()
+                        tfood.clear()
+                        gameover()
+    tscore.clear()
+    tfood.clear()
+    gameover()
+
 
